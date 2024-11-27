e0d11d7b09073c766c5ce4a865ce06c8051af40a
Szazam07 <adam-szarek07@wp.pl>
2024-11-27 12:36:33 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hello and welcome!");
+        System.out.printf("Adam tu był!");
         System.out.printf("Zmiany");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
